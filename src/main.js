d9db7394df66d571fccec31556d44f47dfd6dff3
Antônio Pinheiro <techwoodev@gmail.com>
2024-07-20 19:09:10 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -56,3 +56,27 @@ document.addEventListener("DOMContentLoaded", function() {
         window.addEventListener("orientationChange", lazyload);
     }
 });
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
